83b3f1168056c946891da80b668ceb551e3369fc
firasbouali11 <firas.bouali11@gmail.com>
2021-06-12 11:10:08 +0100

    hethi li5ra

--- Alexa/alexa.py
+++ Alexa/alexa.py
@@ -23,8 +23,9 @@ import struct
 EMAIL = "santal.project10@gmail.com"
 PASSWORD  = "santal123456"
 
-host = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
+#host = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
 
+host = input("enter the server ip address: ")
 ENDPOINT = f"http://{host}:8000/"
 print(ENDPOINT)
 clientsocket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
